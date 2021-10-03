ab81c7e9f7291945a9f89dd0d5d8aa85d4a901d5
Oleksandr Levak <sashavytvyckyj@gmail.com>
2021-10-03 18:57:35 +0300

    lab2_OOP

---
+++ lab2_OOP/src/main/scala/objsets/PostSet.scala
@@ -0,0 +1,151 @@
+package objsets
+
+
+class Post(val user: String, val text: String, val likes: Int) {
+  override def toString: String =
+    "User: " + user + "\n" +
+      "Text: " + text + " [" + likes + "]"
+}
+
+abstract class PostSet extends postsetInterface {
+  def isEmpty: Boolean
+
+  def isEmptyLikes: Int
+
+  def filter(p: Post => Boolean): PostSet = this.filterAcc(p, new Empty)
+
+  def filterAcc(p: Post => Boolean, acc: PostSet): PostSet
+
+  def union(that: PostSet): PostSet
+
+  def mostLiked: Post
+
+  def descendingByLikes: PostList
+
+  def incl(post: Post): PostSet
+
+  def remove(post: Post): PostSet
+
+  def contains(post: Post): Boolean
+
+  def foreach(f: Post => Unit): Unit
+
+}
+
+class Empty extends PostSet {
+
+  def isEmpty: Boolean = true
+  def isEmptyLikes: Int = 0
+
+  def union(that: PostSet): PostSet = that
+
+  def mostLiked: Post = throw new NoSuchElementException
+
+  def descendingByLikes: PostList = Nil
+
+  def contains(post: Post): Boolean = false
+
+  def incl(post: Post): PostSet = new NonEmpty(post, new Empty(), new Empty())
+
+  def remove(post: Post): PostSet = this
+
+  def foreach(f: Post => Unit): Unit = ()
+
+  override def filterAcc(p: Post => Boolean, acc: PostSet): PostSet = acc
+}
+
+class NonEmpty(elem: Post, left: PostSet, right: PostSet) extends PostSet {
+
+  def isEmpty: Boolean = false
+  def isEmptyLikes: Int = mostLiked.likes
+
+  def filterAcc(p: Post => Boolean, acc: PostSet): PostSet =
+    right.filterAcc(p, left.filterAcc(p, if(p(elem)) acc.incl(elem) else acc))
+
+  def union(that: PostSet): PostSet = (left.union(right.union(that))).incl(elem)
+
+  def mostLiked: Post = {
+    if(left.isEmptyLikes > elem.likes || right.isEmptyLikes > elem.likes)
+      if(right.isEmptyLikes > left.isEmptyLikes) right.mostLiked else left.mostLiked
+    else elem
+
+  }
+
+  def descendingByLikes: PostList = {
+    new Cons(mostLiked, remove(mostLiked).descendingByLikes)
+  }
+
+  def contains(x: Post): Boolean =
+    if (x.text < elem.text)
+      left.contains(x)
+    else if (elem.text < x.text)
+      right.contains(x)
+    else true
+
+  def incl(x: Post): PostSet =
+    if (x.text < elem.text)
+     new NonEmpty(elem, left.incl(x), right)
+    else if (elem.text < x.text)
+      new NonEmpty(elem, left, right.incl(x))
+    else
+      this
+
+  def remove(tw: Post): PostSet =
+    if (tw.text < elem.text)
+      new NonEmpty(elem, left.remove(tw), right)
+    else if (elem.text < tw.text)
+      new NonEmpty(elem, left, right.remove(tw))
+    else
+      left.union(right)
+
+  def foreach(f: Post => Unit): Unit = {
+    f(elem)
+    left.foreach(f)
+    right.foreach(f)
+  }
+}
+
+trait PostList {
+  def head: Post
+
+  def tail: PostList
+
+  def isEmpty: Boolean
+
+
+  def foreach(f: Post => Unit): Unit = {
+    if (!isEmpty) {
+      f(head)
+      tail.foreach(f)
+    }
+  }
+}
+
+object Nil extends PostList {
+  def head = throw new java.util.NoSuchElementException("head of EmptyList")
+
+  def tail = throw new java.util.NoSuchElementException("tail of EmptyList")
+
+  def isEmpty = true
+}
+
+class Cons(val head: Post, val tail: PostList) extends PostList {
+  def isEmpty = false
+}
+
+object GoogleVsApple {
+  val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
+  val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
+
+  lazy val googleposts: PostSet = PostReader.allposts.filter(post => google.exists(item => post.text.contains(item)))
+  lazy val appleposts: PostSet = PostReader.allposts.filter(post => apple.exists(item => post.text.contains(item)))
+
+  lazy val trending: PostList = (googleposts.union(appleposts)).descendingByLikes
+
+  def wordsInPosts(words: List[String]): PostSet = PostReader.allposts.filter(post => words.exists(item => post.text.contains(item)))
+}
+
+object Main extends App {
+  // Print the trending posts
+  GoogleVsApple.trending foreach println
+}
---
+++ lab2_OOP/src/test/scala/objsets/PostSetSuite.scala
@@ -0,0 +1,96 @@
+package objsets
+
+import scala.util.Random
+
+class PostSetSuite extends munit.FunSuite {
+
+  val set1 = new Empty()
+  val set2: PostSet = set1.incl(new Post("a", "a body", 20))
+  val set3: PostSet = set2.incl(new Post("b", "b body", 20))
+  val c = new Post("c", "c body", 7)
+  val d = new Post("d", "d body", 9)
+  val set4c: PostSet = set3.incl(c)
+  val set4d: PostSet = set3.incl(d)
+  val set5: PostSet = set4c.incl(d)
+
+  def asSet(posts: PostSet): Set[Post] = {
+    var res = Set[Post]()
+    posts.foreach(res += _)
+    res
+  }
+
+  def size(set: PostSet): Int = asSet(set).size
+
+  val alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  def randStr(n:Int) = (1 to n).map(_ => alpha(Random.nextInt(alpha.length))).mkString
+
+  def randNum(from:Int, to:Int) = Random.between(from, to + 1);
+
+  def randPost(nameLen:Int, postLen:Int, likesRange:Array[Int]) =
+    new Post(randStr(nameLen), randStr(postLen), randNum(likesRange(0), likesRange(1)))
+
+  def randPostSet(n:Int, startPostSet: PostSet = new Empty): PostSet =
+    if(n > 0) randPostSet(n - 1, startPostSet.incl(randPost(5, 10, Array(1, 100))))
+    else startPostSet
+
+  test("filter: on empty set") {
+    assertEquals(size(set1.filter(tw => tw.user == "a")), 0)
+  }
+
+  test("filter: a on set5") {
+      assertEquals(size(set5.filter(tw => tw.user == "a")), 1)
+  }
+
+  test("filter: twenty on set5") {
+      assertEquals(size(set5.filter(tw => tw.likes == 20)), 2)
+  }
+
+  test("union: set4c and set4d") {
+      assertEquals(size(set4c.union(set4d)), 4)
+  }
+
+  test("union: with empty set1") {
+      assertEquals(size(set5.union(set1)), 4)
+  }
+
+  test("union: with empty set2") {
+      assertEquals(size(set1.union(set5)), 4)
+  }
+
+  test("descending: set5") {
+    val trends = set5.descendingByLikes
+    assert(!trends.isEmpty)
+    assert(trends.head.user == "a" || trends.head.user == "b")
+  }
+
+  test("randPostSet: create 200 posts") {
+    assertEquals(size(randPostSet(200)), 200)
+  }
+
+  test("union: 100 posts and 200 posts") {
+    assertEquals(size(randPostSet(200).union(randPostSet(100))), 300)
+  }
+
+  test("GoogleVsApple") {
+    //println(size(GoogleVsApple.googleposts))
+    assertEquals(size(GoogleVsApple.googleposts), 38)
+  }
+
+  test("CEO in posts") {
+    assertEquals(size(GoogleVsApple.wordsInPosts(List("CEO"))), 8)
+  }
+
+  test("Samsung in posts") {
+    assertEquals(size(GoogleVsApple.wordsInPosts(List("Samsung"))), 12)
+  }
+
+  test("Startup and Iran in posts") {
+    assertEquals(size(GoogleVsApple.wordsInPosts(List("Startup", "Iran"))), 11)
+  }
+
+
+
+  import scala.concurrent.duration._
+  override val munitTimeout: FiniteDuration = 10.seconds
+
+}
